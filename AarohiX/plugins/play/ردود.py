6423e663fa34bd306293f9fc63be30f7b259d95e
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-22 17:12:25 +0200

    Add files via upload

---
+++ AarohiX/plugins/play/ردود.py
@@ -0,0 +1,596 @@
+import asyncio
+import random
+from AarohiX import app
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
+from pyrogram import filters, Client
+
+
+
+
+txt = [
+" هاى ماى جايز❤️😉"
+        ]
+@app.on_message(filters.command(["هاي"], ""), group=73)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(txt)
+
+      await message.reply(
+
+        f"{a}")
+        
+        
+thxt = [
+" وعليكم السلام❤️"
+        ]
+@app.on_message(filters.command(["السلام عليكم"], ""), group=173)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(thxt)
+
+      await message.reply(
+
+        f"{a}")        
+     
+htxt = [
+" لو بنت هاجي غير كدة لا 🙄😂"
+        ]
+
+@app.on_message(filters.command(["تعالي خاص"], ""), group=273)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(htxt)
+
+      await message.reply(
+
+        f"{a}")   
+        
+htt = [
+" اعدل وشك يصحبي 🖤 "
+        ]
+
+@app.on_message(filters.command(["🙄"], ""), group=373)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(htt)
+
+      await message.reply(
+
+        f"{a}")           
+        
+htx = [
+" وعليكم السلام ورحمة الله وبركاته 🖤 "
+        ]
+
+@app.on_message(filters.command(["سلام"], ""), group=253)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(htx)
+
+      await message.reply(
+
+        f"{a}")        
+
+ht = [
+" صل الله عليه وسلم 🌝💛"
+        ]
+
+@app.on_message(filters.command(["صلي علي النبي"], ""), group=673)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(ht)
+
+      await message.reply(
+
+        f"{a}")
+
+hxt = [
+" نعم الله عليك 🌚❤️"
+        ]
+
+@app.on_message(filters.command(["نعم"], ""), group=2073)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(txt)
+
+      await message.reply(
+
+        f"{a}")
+
+hytxt = [
+" القمر ده شبهك 🙂❤️"
+        ]
+
+@app.on_message(filters.command(["🌚"], ""), group=2173)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(hytxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+hgtxt = [
+" بتفكر في اي 🤔"
+        ]
+
+@app.on_message(filters.command(["🤔"], ""), group=2573)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(hgtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+ghtxt = [
+" ضحكتك عثل زيكك يصحبي 🌝❤️"
+        ]
+
+@app.on_message(filters.command(["😂"], ""), group=26773)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(ghtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+jhtxt = [
+" متزعلش بحبك 😻🤍"
+        ]
+
+@app.on_message(filters.command(["🥺"], ""), group=26473)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(jhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+ahtxt = [
+" بتعيط لي طيب 😥"
+        ]
+
+@app.on_message(filters.command(["😭"], ""), group=23573)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(ahtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+shtxt = [
+" نا عايز مح انا كمان 🥺💔"
+        ]
+
+@app.on_message(filters.command(["💋"], ""), group=29773)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(shtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+dhtxt = [
+" اعدل وشك ونت بتكلمني 😒? "
+        ]
+
+@app.on_message(filters.command(["😒"], ""), group=2873)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(dhtxt)
+
+      await message.reply(
+
+        f"{a}")
+mhtxt = [
+"في خدك يصحبي 🌝❤️"
+        ]
+
+@app.on_message(filters.command(["محح"], ""), group=2601473)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(mhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+lhtxt = [
+" وانا كمان بعشقك يا يصحبي 🤗🥰"
+        ]
+
+@app.on_message(filters.command(["بحبك"], ""), group=231673)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(lhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+xhtxt = [
+" دايما يصحبي 🌝❤️"
+        ]
+
+@app.on_message(filters.command(["الحمدلله"], ""), group=274683)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(xhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+dfhtxt = [
+" بنهش كتاكيت احنا هنا ولا اي ??"
+        ]
+
+@app.on_message(filters.command(["هشش"], ""), group=2756033)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(dfhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+nhtxt = [
+" هلا بيك يصحبي ♥"
+        ]
+
+@app.on_message(filters.command(["هلا"], ""), group=207973)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(nhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+phtxt = [
+" استغفر الله ♥📿"
+        ]
+
+@app.on_message(filters.command(["بف"], ""), group=270973)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(phtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+ihtxt = [
+" استغفر الله ♥📿"
+        ]
+
+@app.on_message(filters.command(["خاص"], ""), group=273573)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(ihtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+uhtxt = [
+" انت الخير يصحبي 🌝❤️"
+        ]
+
+@app.on_message(filters.command(["بخير"], ""), group=279373)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(uhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+rhtxt = [
+" الشارع الي وراه 😂❤"
+        ]
+
+@app.on_message(filters.command(["اه"], ""), group=267473)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(rhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+htxtk = [
+"وقلبي اتكسر يصحبي 💔 "
+        ]
+
+@app.on_message(filters.command(["حصل"], ""), group=225973)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(htxtk)
+
+      await message.reply(
+
+        f"{a}")
+
+asdhtxt = [
+" اجي فين استغفر الله 📿"
+        ]
+
+@app.on_message(filters.command(["تع"], ""), group=200873)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(asdhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+pokghtxt = [
+" ده نورك ي صحبي 🌝💙"
+        ]
+
+@app.on_message(filters.command(["منور"], ""), group=200173)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(pokghtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+ijkhtxt = [
+" ابشر يحب 😻"
+        ]
+
+@app.on_message(filters.command(["ويت"], ""), group=200273)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(ijkhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+kghtxt = [
+" رايح فين وسايبني 🥺💔"
+        ]
+
+@app.on_message(filters.command(["باي"], ""), group=200373)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(kghtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+lphtxt = [
+" انا علمتك كدا  😒??"
+        ]
+
+@app.on_message(filters.command(["خخخ"], ""), group=200473)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(lphtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+tthtxt = [
+" العفو ياروحي 🙈🌝"
+        ]
+
+@app.on_message(filters.command(["شكرا"], ""), group=200573)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(tthtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+qqhtxt = [
+" انت الي حلو ياقمر 🌝"
+        ]
+
+@app.on_message(filters.command(["حلوه"], ""), group=200673)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(qqhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+wwhtxt = [
+" بعيد الشر عنك يصحبي ♥"
+        ]
+
+@app.on_message(filters.command(["بموت"], ""), group=200773)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(wwhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+zzhtxt = [
+"هو لسا حد طيب? "
+        ]
+
+@app.on_message(filters.command(["تيب"], ""), group=200873)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(zzhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+vvhtxt = [
+" ختك عليه 😂"
+        ]
+
+@app.on_message(filters.command(["اي"], ""), group=200973)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(vvhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+xxhtxt = [
+" حضرلك الخير يارب 🙂❤️"
+        ]
+
+@app.on_message(filters.command(["حاضر"], ""), group=2000173)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(xxhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+cchtxt = [
+" نورت يصحبي ♥"
+        ]
+
+@app.on_message(filters.command(["جيت"], ""), group=2000273)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(cchtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+kjjhtxt = [
+"يوه خضتني يصحبي 🥲"
+        ]
+
+@app.on_message(filters.command(["بخ"], ""), group=2000373)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(kjjhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+ffhtxt = [
+" خلصت روح البعيد يصحبي "
+        ]
+
+@app.on_message(filters.command(["خلاص"], ""), group=2000473)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(ffhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+pphtxt = [
+" مش بحب الكلمه دي يصحبي 🥲♥"
+        ]
+
+@app.on_message(filters.command(["تمام"], ""), group=2000573)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(pphtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+oohtxt = [
+" اوه ياه 🌝😂"
+        ]
+
+@app.on_message(filters.command(["حبيبي"], ""), group=20703)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(oohtxt)
+
+      await message.reply(
+
+        f"{a}")   
+
+llhtxt = [
+" كفايه شقط يبني سيب حاجه لغيرك 😹👅"
+        ]
+
+@app.on_message(filters.command(["سي في"], ""), group=20713)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(llhtxt)
+      
+      await message.reply(
+
+        f"{a}")   
+
+kkhtxt = [
+"كفايه شقط يبني سيب حاجه لغيرك 😹👅"
+        ]
+
+@app.on_message(filters.command(["سيفي"], ""), group=22703)
+
+async def caesar(client: Client, message: Message):
+
+      a = random.choice(kkhtxt)
+
+      await message.reply(
+
+        f"{a}")   
+ 
