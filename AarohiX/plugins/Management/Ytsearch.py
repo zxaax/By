2f6a5f158c7937b995ea88962932f5e9e29458fd
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-22 15:54:50 +0200

    Update Ytsearch.py

--- AarohiX/plugins/Management/Ytsearch.py
+++ AarohiX/plugins/Management/Ytsearch.py
@@ -9,7 +9,7 @@ from config import BOT_USERNAME
 
 
 
-@app.on_message(filters.command("ytsearch"))
+@app.on_message(filters.command("ytsearch",prefixes=""))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
