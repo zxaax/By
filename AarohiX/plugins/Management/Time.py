d23635dc03b25668a5e2436835ffbc5bd59bc441
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-23 02:59:42 +0200

    Update Time.py

--- AarohiX/plugins/Management/Time.py
+++ AarohiX/plugins/Management/Time.py
@@ -5,11 +5,11 @@ from AarohiX import app
 
 
 def get_current_time():
-    tz = pytz.timezone('Asia/Kolkata')  # Setting the timezone to India (Kolkata)
+    tz = pytz.timezone('Africa/Cairo')  # Setting the timezone to Egypt (Cairo)
     current_time = datetime.now(tz)
     return current_time.strftime("%Y-%m-%d %H:%M:%S %Z%z")
 
-@app.on_message(filters.command(["Time"]))
+@app.on_message(filters.command("الساعه كام",prefixes=""))
 def send_time(client, message):
     time = get_current_time()
-    client.send_message(message.chat.id, f"Current time in India: {time}")
+    client.send_message(message.chat.id, f"الوقت الحالي في القاهره: {time}")
