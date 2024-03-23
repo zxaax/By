6a3085169871062f9b2a0c4d61c538d1e75b82c7
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-23 15:54:39 +0200

    Update welcome.py

--- AarohiX/plugins/Management/welcome.py
+++ AarohiX/plugins/Management/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from AarohiX.utils.daxx_ban import admin_filter
+import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
