fd89d7a9ac90bad3d23e087466ff51b3bc8a4d88
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-26 03:47:08 +0200

    Update must_join.py

--- AarohiX/plugins/play/must_join.py
+++ AarohiX/plugins/play/must_join.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from AFROTOMusic import app
+from AarohiX import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
