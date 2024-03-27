2d4b559e7f3605680884fac7c9f35e9125a6fbff
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-27 02:27:13 +0200

    Update play.py

--- AarohiX/plugins/play/play.py
+++ AarohiX/plugins/play/play.py
@@ -29,7 +29,7 @@ from config import BANNED_USERS, lyrical
     command(
         [
             "تشغيل",
-            "vplay",
+            "فيديو",
             "cplay",
             "cvplay",
             "playforce",
