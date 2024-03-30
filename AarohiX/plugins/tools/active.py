25a05d09afe1b4d667dadaf8f85528c0598bca32
ســورس تيــتو <157909403+DevTeto@users.noreply.github.com>
2024-03-30 17:52:23 +0200

    Update active.py

--- AarohiX/plugins/tools/active.py
+++ AarohiX/plugins/tools/active.py
@@ -12,7 +12,7 @@ from AarohiX.utils.database import (
 )
 
 
-@app.on_message(filters.command("المكالمات النشطه",prefixes="")) & SUDOERS)
+@app.on_message(command(["الاتصالات", "activevoice"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text("~ جاري البحث عن اتصالات .")
     served_chats = await get_active_chats()
@@ -44,7 +44,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command("الفديوهات النشطه",prefixes="")) & SUDOERS)
+@app.on_message(command(["الفيديوات", "activevideo"]) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text("~ جاري البحث عن فيديوات متوفرة .")
     served_chats = await get_active_video_chats()
