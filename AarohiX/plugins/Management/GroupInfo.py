38d982f8785620a5a246d3b4151843055fdc6923
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-03-31 23:52:58 +0300

    Update GroupInfo.py

--- AarohiX/plugins/Management/GroupInfo.py
+++ AarohiX/plugins/Management/GroupInfo.py
@@ -2,10 +2,10 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from AarohiX import app
 
-@app.on_message(filters.command("معلومات المجموعه", prefixes=""))
+@app.on_message(filters.command("معلومات المجموعة", prefixes=""))
 async def get_group_status(_, message: Message):
     if len(message.command) != 2:
-        await message.reply("الرجاء تقديم اسم مستخدم المجموعة. مثال: `معلومات المجموعه @T7_AU`")
+        await message.reply("الرجاء تقديم اسم مستخدم المجموعة. مثال: `معلومات المجموعة Tepthon_Help`")
         return
     
     group_username = message.command[1]
@@ -13,7 +13,7 @@ async def get_group_status(_, message: Message):
     try:
         group = await app.get_chat(group_username)
     except Exception as e:
-        await message.reply(f"Error: {e}")
+        await message.reply(f"خطأ: {e}")
         return
     
     total_members = await app.get_chat_members_count(group.id)
@@ -21,24 +21,24 @@ async def get_group_status(_, message: Message):
     premium_acc = banned = deleted_acc = bot = 0  # You should replace these variables with actual counts.
 
     response_text = (
-        f"➖➖➖➖➖➖➖\n"
-        f"➲ أسم المجموعة : {group.title} ✅\n"
+        f"↢ معلومات المجموعة : \n"
+        f"➲ اسم المجموعة : {group.title} \n"
         f"➲ معرف مجموعة : {group.id}\n"
         f"➲ إجمالي الأعضاء : {total_members}\n"
         f"➲ البايو : {group_description or 'N/A'}\n"
         f"➲ اليوزر : @{group_username}\n"
        
-        f"➖➖➖➖➖➖➖"
+        f"𝐅𝐀𝐇𝐄𝐃 🇵🇸🤍"
     )
     
     await message.reply(response_text)
 
-@app.on_message(filters.command("status") & filters.group)
+@app.on_message(filters.command("نوع المجموعة") & filters.group)
 def group_status(client, message):
     chat = message.chat
-    status_text = f"Group ID: {chat.id}\n" \
-                  f"Title: {chat.title}\n" \
-                  f"Type: {chat.type}\n"
+    status_text = f"أيدي المجموعة: {chat.id}\n" \
+                  f"اسم المجموعة : {chat.title}\n" \
+                  f"النوع : {chat.type}\n"
                   
     if chat.username:
         status_text += f"Username: @{chat.username}"
