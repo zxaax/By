c453003dbe08ece53873dd363bc7f7296717564c
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-04-03 18:58:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RRYR7/QuranEzaa",
+    "https://github.com/RRYR7/YTYSOURCE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
