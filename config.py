2c6a64b5d1035f4935164bcefaa4122688d3f741
PYYRQ <165933124+PYYRQ@users.noreply.github.com>
2024-04-03 19:17:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RRYR7/YTYSOURCE",
+    "https://github.com/PYYRQ/BOT-NY",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
