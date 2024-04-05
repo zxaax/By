0c8236070c445a5dbad925e68db578e2e1fa36bc
PYYRQ <165933124+PYYRQ@users.noreply.github.com>
2024-04-05 09:40:03 +0300

    Update must_join.py

--- AarohiX/plugins/tools/must_join.py
+++ AarohiX/plugins/tools/must_join.py
@@ -5,28 +5,28 @@ from AarohiX import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/WX_PM":  # Not compulsory
+    if not "https://t.me/Tepthon":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("WX_PM", msg.from_user.id)
+            await bot.get_chat_member("Tepthon", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/WX_PM".isalpha():
-                link = "https://t.me/WX_PM"
+            if "https://t.me/Tepthon".isalpha():
+                link = "https://t.me/Tepthon"
             else:
-                chat_info = await bot.get_chat("WX_PM")
+                chat_info = await bot.get_chat("Tepthon")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @WX_PM .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذرًا عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت أولًا\n⌯︙قناة البوت : @Tepthon .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("تحديثات سهيله 🧚🏻", url=link)]
+                        [InlineKeyboardButton("تحديثات فهد 🇵🇸", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I m not admin in the MUST_JOIN chat @WX_PM !")
+        print(f"I m not admin in the MUST_JOIN chat @Tepthon !")
       
