136836f6c2c7475a0311b6ef712f5ed7df4b5713
PYYRQ <165933124+PYYRQ@users.noreply.github.com>
2024-06-07 09:20:41 +0300

    Update Gpt.py

--- AarohiX/plugins/Management/Gpt.py
+++ AarohiX/plugins/Management/Gpt.py
@@ -15,7 +15,7 @@ async def chat_gpt(bot, message):
 
         if len(message.command) < 2:
             await message.reply_text(
-                "• استخدم الأمر هكذا :\n\n • الذكاء الاصنتاعي + سؤالك"
+                "• استخدم الأمر هكذا :\n\n • الذكاء الاصطناعي + سؤالك"
             )
         else:
             a = message.text.split(' ', 1)[1]
