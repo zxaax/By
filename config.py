7fa0bf30363b8abd8c7cb9a91c7c6522ef3a010c
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-07 22:05:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/PYYRQ/Bb",
+    "https://github.com/zxaax/Bb",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
