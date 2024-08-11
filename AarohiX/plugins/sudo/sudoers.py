f3dc688425ffc15b0068f2abe49ca38a88f0a95d
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-11 18:56:44 +0300

    Update sudoers.py

--- AarohiX/plugins/sudo/sudoers.py
+++ AarohiX/plugins/sudo/sudoers.py
@@ -10,7 +10,7 @@ from AarohiX.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 from strings.filters import command
 
-@app.on_message(command(["رفع منشئ"]) & filters.user(OWNER_ID))
+@app.on_message(command(["ر...فع منشئ"]) & filters.user(OWNER_ID))
 @language
 async def useradd(client, message: Message, _):
     if not message.reply_to_message:
