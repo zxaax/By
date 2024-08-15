80cd50e7626b8fa9afd4ad804beccc40a6f8208e
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-15 18:04:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -25,9 +25,9 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "20971520")
 )
 
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002208963284"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002125199203"))
 
-OWNER_ID = int(getenv("OWNER_ID", "6627097100"))
+OWNER_ID = int(getenv("OWNER_ID", "6766862102"))
 
 BOT_USERNAME = getenv("BOT_USERNAME" , "")
 
@@ -39,15 +39,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/zxaax/Bb",
+    "https://github.com/zxaax/By",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tmemgqa10")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nlla0090")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/iioooou")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/daewaAhlAlbayt")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
